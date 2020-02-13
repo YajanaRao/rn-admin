2d4f3a4b730dc703751a5250d01aadf2bb6ff87b
YajanaRao <yajananrao@gmail.com>
2020-02-13 17:50:18 +0530

    IOS

--- ios/RNAdminTests/RNAdminTests.m
+++ ios/RNAdminTests/RNAdminTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface MyAppTests : XCTestCase
+@interface RNAdminTests : XCTestCase
 
 @end
 
-@implementation MyAppTests
+@implementation RNAdminTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
